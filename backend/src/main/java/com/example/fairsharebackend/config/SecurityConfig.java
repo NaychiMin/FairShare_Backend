16c85377f64ec7b22cba6bcddb9ff79057c14c3d
NaychiMin <naychimin02@gmail.com>
2026-02-22 15:54:57 +0800

    Add expense endpoints to csrf

--- backend/src/main/java/com/example/fairsharebackend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/fairsharebackend/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/group/**"
+                                "/group/**",
+                                "/expenses/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
