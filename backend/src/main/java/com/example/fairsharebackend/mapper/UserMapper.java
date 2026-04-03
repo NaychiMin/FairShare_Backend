99d27ec2aeab4b144ce85eba090408ba012687d4
micaellaa <99943347+micaellaa@users.noreply.github.com>
2026-04-03 23:54:59 +0800

    Make login user DTO more lean

--- backend/src/main/java/com/example/fairsharebackend/mapper/UserMapper.java
+++ backend/src/main/java/com/example/fairsharebackend/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.example.fairsharebackend.mapper;
 import com.example.fairsharebackend.entity.User;
 import com.example.fairsharebackend.entity.dto.request.UserRegisterRequestDto;
 import com.example.fairsharebackend.entity.dto.request.UserUpdateRequestDto;
+import com.example.fairsharebackend.entity.dto.response.UserDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
@@ -10,6 +11,6 @@ import org.mapstruct.MappingTarget;
 public interface UserMapper {
 
     User toEntity(UserRegisterRequestDto request);
-
+    UserDto toDto(User request);
     void updateFromDto(UserUpdateRequestDto dto, @MappingTarget User user);
 }
