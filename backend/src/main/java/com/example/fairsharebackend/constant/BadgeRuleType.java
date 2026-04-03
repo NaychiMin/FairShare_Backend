be519910b0da5cf453c9908c19db8e524c939ff4
micaellaa <99943347+micaellaa@users.noreply.github.com>
2026-04-03 23:07:03 +0800

    Add data.sql with badges, group_roles, static_roles

--- backend/src/main/java/com/example/fairsharebackend/constant/BadgeRuleType.java
+++ backend/src/main/java/com/example/fairsharebackend/constant/BadgeRuleType.java
@@ -3,7 +3,6 @@ package com.example.fairsharebackend.constant;
 public enum BadgeRuleType {
     // Settlement
     SETTLEMENT_COUNT,
-
     // Expense
     EXPENSE_COUNT,
     // Settlement
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,40 @@
+-- STATIC ROLES
+INSERT INTO `fairshare`.`static_role`
+(`static_role_id`,
+`name`)
+VALUES
+(
+    UUID_TO_BIN('11111111-1111-1111-1111-111111111111'),
+    'ADMIN'
+),
+(
+    UUID_TO_BIN('22222222-2222-2222-2222-222222222222'),
+    'USER'
+)
+ON DUPLICATE KEY UPDATE
+name = VALUES(name);
+
+-- GROUPS
+INSERT INTO `fairshare`.`tb_role`
+(`role_id`,
+`name`,
+`scope`)
+VALUES
+(
+    1,
+    'GROUP_ADMIN',
+    'GROUP'
+),
+(
+    2,
+    'GROUP_MEMBER',
+    'GROUP'
+)
+ON DUPLICATE KEY UPDATE
+name = VALUES(name),
+scope = VALUES(scope);
+
+-- BADGES
 INSERT INTO `fairshare`.`tb_badge`
 (`badge_id`,
 `badge_rule_type`,
@@ -8,20 +45,24 @@ INSERT INTO `fairshare`.`tb_badge`
 `rule_config`)
 VALUES
 (
-    UUID_TO_BIN(UUID()),
-    0,  -- SETTLEMENT
+    UUID_TO_BIN('33333333-3333-3333-3333-333333333333'),
+    2,  -- SETTLEMENT_TIMING
     1,  -- GROUP (1 based on enum order if PERSONAL=0, GROUP=1)
-    0,  -- SETTLEMENT_COUNT
-    'Yay! got 3',
-    '3 SETTLEMENTS',
-    '{"count": 3}'
+    0,  -- SETTLEMENT
+    'You have made a settlement 30 minutes after the last balance!',
+    'Early Bird',
+    '{"timingInMin": 30}'
 ),
 (
-    UUID_TO_BIN(UUID()),
-    1,  -- EXPENSE
-    1,  -- GROUP (1 based on enum order if PERSONAL=0, GROUP=1)
+    UUID_TO_BIN('44444444-4444-4444-4444-444444444444'),
     1,  -- EXPENSE_COUNT
-    'Yay! got 3',
-    '3 EXPENSES',
-    '{"count": 3}'
-);
+    1,  -- GROUP (1 based on enum order if PERSONAL=0, GROUP=1)
+    1,  -- EXPENSE
+    'You have added 5 expenses today!',
+    'Record Keeper',
+    '{"count": 5}'
+)
+ON DUPLICATE KEY UPDATE
+description = VALUES(description),
+name = VALUES(name),
+rule_config = VALUES(rule_config);
